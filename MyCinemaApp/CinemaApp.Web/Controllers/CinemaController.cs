4fbd658c031fc9738d68de306f09bfd66154e78a
Slavi Bozhilov <slavib2002@abv.bg>
2024-09-27 20:47:06 +0300

    AddedCinemaView

---
+++ MyCinemaApp/CinemaApp.Web/Controllers/CinemaController.cs
@@ -0,0 +1,21 @@
+﻿using CinemaApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class CinemaController(CinemaDbContext context) : Controller
+    {
+        public  IActionResult Index()
+        {
+            var cinemas=context
+                .Cinemas
+                .OrderBy (c => c.Location)
+                .ThenBy (c => c.Name)
+                .ToList();
+
+
+            return View(cinemas);
+        }
+    }
+}
